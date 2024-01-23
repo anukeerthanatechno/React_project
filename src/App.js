b49b345514bc64d1968f1202454f7b3bc0c429f0
anukeerthanatechno <149554165+anukeerthanatechno@users.noreply.github.com>
2024-01-23 10:55:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>My react js app</h1>
-      <h1>My react js app</h1>
       <h1>My react js appsss</h1>
       <h1>New react project</h1>
-      <h1>New react project</h1>
-          <h1>New react app</h1>
-
+      <h1>New easy react app</h1>
     <h3>new line</h3>
     <h1>New react project</h1>
     </div>
